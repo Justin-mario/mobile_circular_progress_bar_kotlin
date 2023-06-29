d6b3081a5bf77f24873342fd68c1d71c210863f3
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-06-29 23:01:22 +0100

    first commit

---
+++ app/src/main/java/com/example/android/circularprogressbar/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.android.circularprogressbar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.android.circularprogressbar.ui.theme.CircularProgressBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CircularProgressBarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    CircularProgressBar()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0,
+    ) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CircularProgressBarTheme {
+//        CircularProgressBar()
+    }
+}
